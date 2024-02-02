c48253143620bb83d301282194a7ec1ed9e5fb63
tanusumra <141604389+tanusumra@users.noreply.github.com>
2024-02-02 08:34:52 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,15 @@
+class sum
+{
+	public static void main(String[] args) 
+	{
+		int a = 5;
+        	int b = 10;
+        	int c = 15;
+		
+		int sum = a + b + c;
+		double average = (double) sum / 3;
+
+        	System.out.println("Sum: " + sum);
+        	System.out.println("Average: " + average);
+    	}
+}
