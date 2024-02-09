4cc18fc92c6bbdc2b3fcc21ba3fd98791bc364b3
tanusumra <141604389+tanusumra@users.noreply.github.com>
2024-02-09 19:44:34 +0500

    Add files via upload

---
+++ LeftHalf.java
@@ -0,0 +1,31 @@
+
+import java.util.*;
+
+public class LeftHalf {
+	
+	public static void printPattern(int n)
+	{
+		int i, j;
+
+		
+		for (i = n; i >= 1; i--) {
+
+			
+			for (j = 1; j < i; j++) {
+				System.out.print(" ");
+			}
+
+			for (j = 0; j <= n - i; j++) {
+				System.out.print("*");
+			}
+
+			System.out.println();
+		}
+	}
+
+	public static void main(String args[])
+	{
+		int n = 6;
+		printPattern(n);
+	}
+}
